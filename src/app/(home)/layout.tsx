e41833020e09e0591b31b2a0aad4c7c21bb08ad0
Алексей Бельченков <u608110@gmail.com>
2025-07-21 13:04:11 +0400

    home sidebar: menu & logo

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
+
 interface LayoutProps {
 	children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps)=> {
 	return (
-		<div>{children}</div>
+		<HomeLayout>
+			{children}
+		</HomeLayout>
 	);
 }
 
