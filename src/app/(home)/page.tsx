584b0d8700162886564535b399e04469b7da1726
Алексей Бельченков <u608110@gmail.com>
2025-07-27 17:19:25 +0400

    home navbar: fix collapsible

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,13 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-        <Image
-          src="/logo.svg"
-          alt="logo"
-          width={50}
-          height={50}
-        />
-        <p className="text-xl font-semibold tracking-tight">YouTube</p>
+       I will load videos in the future!
     </div>
   );
 }
