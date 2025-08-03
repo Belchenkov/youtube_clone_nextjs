d2cd4df3193d0485b3c118ae24260724a3e28b22
Алексей Бельченков <u608110@gmail.com>
2025-08-03 09:57:03 +0400

    protected routes

---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+       Only logged in users should see this.
+    </div>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+	'/protected(.*)',
+]);
+
+export default clerkMiddleware(async (auth, request, event) => {
+	if (isProtectedRoute(request)) {
+		await auth.protect();
+	}
+});
 
 export const config = {
 	matcher: [
