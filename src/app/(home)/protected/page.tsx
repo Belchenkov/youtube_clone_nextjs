02090357de8c0743e5125ad03d6482d98aeef4ce
Алексей Бельченков <u608110@gmail.com>
2025-08-26 13:24:41 +0400

    Home View component

--- src/app/(home)/protected/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div>
-       Only logged in users should see this.
-    </div>
-  );
-}
---
+++ src/modules/home/ui/sections/home-videos-section.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomeVideosSection = () => {
+	return (
+		<div>
+			HomeVideosSection
+		</div>
+	);
+};
+
+export default HomeVideosSection;
