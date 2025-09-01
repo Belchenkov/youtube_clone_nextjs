f2f5136e0ca992e183854884e7f9ace0b204159a
Алексей Бельченков <u608110@gmail.com>
2025-09-01 12:08:15 +0400

    Fix auth button for user profile studio

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,19 +1,29 @@
-import { UserCircleIcon } from "lucide-react";
-import {
-	UserButton,
-	SignInButton,
-	SignedIn,
-	SignedOut,
-} from "@clerk/nextjs";
+"use client";
 
-import { Button } from "@/components/ui/button";
+import { ClapperboardIcon, UserCircleIcon, UserIcon } from "lucide-react"
+import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button"
 
 export const AuthButton = () => {
 	return (
 		<>
 			<SignedIn>
-				<UserButton />
-			{/* Add menu items for Studio and User profile */}
+				<UserButton>
+					<UserButton.MenuItems>
+						<UserButton.Link
+							label="My profile"
+							href="/users/current"
+							labelIcon={<UserIcon className="size-4" />}
+						/>
+						<UserButton.Link
+							label="Studio"
+							href="/studio"
+							labelIcon={<ClapperboardIcon className="size-4" />}
+						/>
+						<UserButton.Action label="manageAccount" />
+					</UserButton.MenuItems>
+				</UserButton>
 			</SignedIn>
 			<SignedOut>
 				<SignInButton mode="modal">
@@ -27,5 +37,5 @@ export const AuthButton = () => {
 				</SignInButton>
 			</SignedOut>
 		</>
-	);
-};
+	)
+}
