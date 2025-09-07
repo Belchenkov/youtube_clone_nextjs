75d4424d7932031570fac367dfd2ec819f882bc9
Алексей Бельченков <u608110@gmail.com>
2025-09-07 15:48:36 +0400

    Init studio upload modal component

---
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export const StudioUploadModal = () => {
+  return (
+    <>
+      <Button variant="secondary">
+        Create
+      </Button>
+    </>
+  );
+};
