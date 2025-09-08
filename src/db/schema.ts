965d4c556b64efb9269fea2ecc4676f5ec1c8170
Алексей Бельченков <u608110@gmail.com>
2025-09-08 11:47:09 +0400

    Add videos scheme

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,12 @@
-import { uuid, pgTable, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core";
+import {
+	uuid,
+	pgTable,
+	text,
+	timestamp,
+	uniqueIndex,
+	integer,
+	pgEnum,
+} from "drizzle-orm/pg-core";
 export const users = pgTable("users", {
  	id: uuid('id').primaryKey().defaultRandom(),
 	clerkId: text('clerk_id').unique().notNull(),
@@ -20,3 +28,34 @@ export const categories = pgTable("categories", {
 }, (t) => [
 	uniqueIndex('name_idx').on(t.name),
 ]);
+
+export const videoVisibility = pgEnum("video_visibility", [
+	"private",
+	"public",
+]);
+
+export const videos = pgTable("videos", {
+	id: uuid("id").primaryKey().defaultRandom(),
+	title: text("title").notNull(),
+	description: text("description"),
+	muxStatus: text("mux_status"),
+	muxAssetId: text("mux_asset_id").unique(),
+	muxUploadId: text("mux_upload_id").unique(),
+	muxPlaybackId: text("mux_playback_id").unique(),
+	muxTrackId: text("mux_track_id").unique(),
+	muxTrackStatus: text("mux_track_status"),
+	thumbnailUrl: text("thumbnail_url"),
+	thumbnailKey: text("thumbnail_key"),
+	previewUrl: text("preview_url"),
+	previewKey: text("preview_key"),
+	duration: integer("duration").default(0).notNull(),
+	visibility: videoVisibility("visibility").default("private").notNull(),
+	userId: uuid("user_id").references(() => users.id, {
+		onDelete: "cascade",
+	}).notNull(),
+	categoryId: uuid("category_id").references(() => categories.id, {
+		onDelete: "set null",
+	}),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
