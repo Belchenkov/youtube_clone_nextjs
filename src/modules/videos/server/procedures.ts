f41baa70bbabf3b48bef7f502b85fc784882d077
Алексей Бельченков <u608110@gmail.com>
2025-09-12 10:20:41 +0400

    crete video procedures

---
+++ src/modules/videos/server/procedures.ts
@@ -0,0 +1,21 @@
+import { db } from "@/db";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { videos  } from "@/db/schema";
+
+export const videosRouter = createTRPCRouter({
+  create: protectedProcedure.mutation(async ({ ctx }) => {
+    const { id: userId } = ctx.user;
+
+    const [video] = await db
+      .insert(videos)
+      .values({
+        userId,
+        title: "Untitled",
+      })
+      .returning();
+
+    return {
+      video: video,
+    };
+  }),
+});
