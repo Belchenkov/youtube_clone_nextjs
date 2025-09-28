210b0c896efeb0f1b75d95d31e5e82289cffc63b
Алексей Бельченков <u608110@gmail.com>
2025-09-28 10:06:47 +0400

    install drizzle zod

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -8,6 +8,12 @@ import {
 	pgEnum,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm";
+import {
+	createInsertSchema,
+	createSelectSchema,
+	createUpdateSchema,
+} from "drizzle-zod";
+
 export const users = pgTable("users", {
  	id: uuid('id').primaryKey().defaultRandom(),
 	clerkId: text('clerk_id').unique().notNull(),
@@ -68,6 +74,11 @@ export const videos = pgTable("videos", {
 	createdAt: timestamp("created_at").defaultNow().notNull(),
 	updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
+
+
+export const videoSelectSchema = createSelectSchema(videos);
+export const videoInsertSchema = createInsertSchema(videos);
+export const videoUpdateSchema = createUpdateSchema(videos);
 
 export const videoRelations = relations(videos, ({ one }) => ({
 	user: one(users, {
