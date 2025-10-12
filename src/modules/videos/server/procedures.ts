7133a0da1351fa795690b7b80170d09e680b4130
Алексей Бельченков <u608110@gmail.com>
2025-10-12 10:37:57 +0400

    add generate title method to video procedures

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -13,6 +13,18 @@ import {
 } from "@/db/schema";
 
 export const videosRouter = createTRPCRouter({
+	generateTitle: protectedProcedure
+		.input(z.object({ id: z.string().uuid() }))
+		.mutation(async ({ ctx, input }) => {
+			const { id: userId } = ctx.user;
+
+			const { workflowRunId } = await workflow.trigger({
+				url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/title`,
+				body: { userId, videoId: input.id },
+			});
+
+			return workflowRunId;
+		}),
 	generateThumbnail: protectedProcedure
 		.input(z.object({ id: z.string().uuid(), prompt: z.string().min(10) }))
 		.mutation(async ({ ctx, input }) => {
