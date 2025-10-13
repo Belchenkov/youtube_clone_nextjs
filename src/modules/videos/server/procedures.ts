ee61ccafa7b9dd539c3695f02cbfb658bd978b10
Алексей Бельченков <u608110@gmail.com>
2025-10-13 09:30:53 +0400

    add generate description method to video procedures

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -13,6 +13,18 @@ import {
 } from "@/db/schema";
 
 export const videosRouter = createTRPCRouter({
+	generateDescription: protectedProcedure
+		.input(z.object({ id: z.string().uuid() }))
+		.mutation(async ({ ctx, input }) => {
+			const { id: userId } = ctx.user;
+
+			const { workflowRunId } = await workflow.trigger({
+				url: `${process.env.UPSTASH_WORKFLOW_URL}/api/videos/workflows/description`,
+				body: { userId, videoId: input.id },
+			});
+
+			return workflowRunId;
+		}),
 	generateTitle: protectedProcedure
 		.input(z.object({ id: z.string().uuid() }))
 		.mutation(async ({ ctx, input }) => {
