3da38a3747bb4372a987e6d927b3a5406665959b
Алексей Бельченков <u608110@gmail.com>
2025-11-09 15:03:14 +0400

    Add commentsRouter with CRUD operations and integrate into appRouter

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,12 +3,14 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
+import { commentsRouter } from '@/modules/comments/server/procedures';
 
 export const appRouter = createTRPCRouter({
 	categories: categoriesRouter,
 	studio: studioRouter,
 	videos: videosRouter,
 	videoViews: videoViewsRouter,
+	comments: commentsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
