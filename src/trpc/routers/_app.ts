dc531f53a06a686e6ca7d7ad9adda839b2affc2a
Алексей Бельченков <u608110@gmail.com>
2025-11-12 11:08:07 +0400

    Integrate `subscriptionsRouter` and `videoReactionsRouter` into `appRouter`

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,12 +4,16 @@ import { studioRouter } from "@/modules/studio/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { commentsRouter } from '@/modules/comments/server/procedures';
+import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
+import { videoReactionsRouter } from "@/modules/procedures";
 
 export const appRouter = createTRPCRouter({
 	categories: categoriesRouter,
 	studio: studioRouter,
 	videos: videosRouter,
 	videoViews: videoViewsRouter,
+	videoReactions: videoReactionsRouter,
+	subscriptions: subscriptionsRouter,
 	comments: commentsRouter,
 });
 // export type definition of API
