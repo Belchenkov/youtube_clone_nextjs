4bf1cc2db3706757f5b659ee903ce418db9d2a62
Алексей Бельченков <u608110@gmail.com>
2025-11-24 09:36:34 +0400

    Refactor `trpc/client` to use `APP_URL` for URL resolution and improve code readability.

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -1,14 +1,14 @@
 'use client';
 // ^-- to make sure we can mount the Provider from a server component
 import type { QueryClient } from '@tanstack/react-query';
+import superjson from "superjson";
 import { QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
-import superjson from 'superjson';
-
 import { httpBatchLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
+import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+import { APP_URL } from '@/constants';
 export const trpc = createTRPCReact<AppRouter>();
 let clientQueryClientSingleton: QueryClient;
 function getQueryClient() {
@@ -22,9 +22,8 @@ function getQueryClient() {
 function getUrl() {
 	const base = (() => {
 		if (typeof window !== 'undefined') return '';
-		// TODO: Modify for outside-vercel deployment
-		if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
-		return 'http://localhost:3000';
+		// Crucial to modify in .env to production domain (including protocol)
+		return APP_URL;
 	})();
 	return `${base}/api/trpc`;
 }
@@ -46,18 +45,18 @@ export function TRPCProvider(
 					url: getUrl(),
 					async headers() {
 						const headers = new Headers();
-						headers.set('x-trpc-source', 'nextjs-react');
+						headers.set("x-trpc-source", "nextjs-react");
 						return headers;
-					},
+					}
 				}),
 			],
 		}),
 	);
 	return (
 		<trpc.Provider client={trpcClient} queryClient={queryClient}>
-		<QueryClientProvider client={queryClient}>
-			{props.children}
-		</QueryClientProvider>
+			<QueryClientProvider client={queryClient}>
+				{props.children}
+			</QueryClientProvider>
 		</trpc.Provider>
-);
+	);
 }
